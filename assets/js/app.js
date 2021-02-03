3ff8f0b7311eeaa343913951f5624923b7a6c586
tddthinh <tddthinh@gmail.com>
2021-02-03 16:47:26 +0700

    Revert "commit XXX with Tortoise"
    This reverts commit b3ed621c02c124f565bfcef4c32dec658e118648.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 $("#run").click(function () {
     var n = $("#input-number").val();
     if(n % 3 === 0 && n % 5 === 0) {
-        $("#output").text("FizzBuzz XXX");
+        $("#output").text("FizzBuzz");
     }
     else if(n % 3 === 0) {
         $("#output").text("Fizz");
